817463ff3fcf7367642c10e6e1a270ef4ea9b4a8
1moalim1all1-cyber <1moalim1all1@gmail.com>
2026-07-28 00:35:37 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,19 @@
-// انسخ بيانات Firebase Web App هنا
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-storage.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAJTOIvwirrWrli24x07PCyGKqG5LJAPJE",
+  authDomain: "alamal-86bd1.firebaseapp.com",
+  projectId: "alamal-86bd1",
+  storageBucket: "alamal-86bd1.firebasestorage.app",
+  messagingSenderId: "487151122464",
+  appId: "1:487151122464:web:b3c8e416a9072bdf6e2445",
+  measurementId: "G-9YW4WXEJFJ"
 };
-export const firebaseEnabled = Boolean(firebaseConfig.apiKey && firebaseConfig.projectId);
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
