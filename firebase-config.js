9fbd6e34e4497219a8575d2e645cc90111ee64cc
7460077mqhmoud <124037248+7460077mqhmoud@users.noreply.github.com>
2026-07-28 01:31:09 +0300

    Update admin panel

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-storage.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
-
 export const firebaseConfig = {
   apiKey: "AIzaSyAJTOIvwirrWrli24x07PCyGKqG5LJAPJE",
   authDomain: "alamal-86bd1.firebaseapp.com",
@@ -13,7 +8,9 @@ export const firebaseConfig = {
   measurementId: "G-9YW4WXEJFJ"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-export const auth = getAuth(app);
+export const firebaseEnabled = Boolean(
+  firebaseConfig.apiKey &&
+  firebaseConfig.authDomain &&
+  firebaseConfig.projectId &&
+  firebaseConfig.appId
+);
